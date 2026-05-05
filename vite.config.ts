cc995832f44739c69c5b7963b5bfb4f6ee5891ad
Fluxo Digital Tech <fluxodigitaltech@gmail.com>
2026-05-04 21:16:33 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import dyadComponentTagger from "@dyad-sh/react-vite-component-tagger";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig(() => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [dyadComponentTagger(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    target: 'es2020', // Garante que o JavaScript seja compilado para uma versão moderna
+  },
+}));
